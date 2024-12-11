e15ca8b91bb9451061ea27275ea127ba54485023
Larry-Lozoya <167731449+Larry-Lozoya@users.noreply.github.com>
2024-12-11 02:12:36 -0700

    database insert
    Database insert is now working for Level one do matching next..

--- app/src/main/java/com/example/languageapp/DatabaseHelper.kt
+++ app/src/main/java/com/example/languageapp/DatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.languageapp
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
@@ -25,5 +26,14 @@ class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         TODO("Not yet implemented")
+    }
+    fun putItemsIntoDB(engWord: String, spanWord: String, correct: Int){
+        val sqLiteDatabase = this.readableDatabase
+        val queryInsert = ContentValues().apply {
+            put("englishWord", engWord)
+            put("spanishWord", spanWord)
+            put("isCorrect", correct)
+        }
+        sqLiteDatabase.insert("translation", null, queryInsert)
     }
 }
--- app/src/main/java/com/example/languageapp/SpanishLanguageLevelOneActivity.kt
+++ app/src/main/java/com/example/languageapp/SpanishLanguageLevelOneActivity.kt
@@ -1,7 +1,12 @@
 package com.example.languageapp
 
+import android.database.Cursor
 import android.os.Bundle
+import android.os.LocaleList
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,9 +21,16 @@ import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.Locale
+
 
 class SpanishLanguageLevelOneActivity : AppCompatActivity() {
     private val ourInputs = arrayListOf("yes", "library", "where", "dog", "cat", "red", "green", "blue")
+    private lateinit var spanishInput: EditText
+    private lateinit var englishTarget: TextView
+    private lateinit var databaseHelper: DatabaseHelper
+    private lateinit var cursor: Cursor
+    private var index = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,57 +41,74 @@ class SpanishLanguageLevelOneActivity : AppCompatActivity() {
             insets
         }
         println(ourInputs.size)
-        var index = 0
-        while(true){
+        spanishInput = findViewById(R.id.actOneEditText)
+        // https://stackoverflow.com/questions/5715072/change-android-keyboard-language
+        // This makes it so our keyboard will show up as a spanish keyboard first we still have
+        // to manually get the spanish keyboard from our settings though
+        spanishInput.setImeHintLocales(LocaleList(Locale("es", "MX")))
+        englishTarget = findViewById(R.id.actOneText)
+        databaseHelper = DatabaseHelper(applicationContext)
 
-            if(index >= ourInputs.size){
-                break
-            }else{
-                println(ourInputs[index])
-                ++index
-            }
-        }
-        fetchJoke()
+
+        fetchOurSpanishWords()
+
+
 
     }
-    private fun fetchJoke() {
+    private fun fetchOurSpanishWords() {
         CoroutineScope(Dispatchers.IO).launch {
-            var result = ""
-            var httpURLConnection : HttpURLConnection? = null
-            try {
-                var url = URL("https://api.mymemory.translated.net/get?q=mom&langpair=en|es")
-                httpURLConnection = url.openConnection() as HttpURLConnection
+            while (true) {
+                if(index >= ourInputs.size){
+                    break
+                }else {
+                    val eachWord = ourInputs[index]
+//                    println(eachWord)
+                    var result = ""
+                    var httpURLConnection: HttpURLConnection? = null
+                    try {
+                        var url =
+                            URL("https://api.mymemory.translated.net/get?q=$eachWord&langpair=en|es")
+                        httpURLConnection = url.openConnection() as HttpURLConnection
 
-                httpURLConnection.requestMethod = "GET"
+                        httpURLConnection.requestMethod = "GET"
 
-                if(httpURLConnection.responseCode != HttpURLConnection.HTTP_OK){
-                    result = "BAD CONNECTION"
+                        if (httpURLConnection.responseCode != HttpURLConnection.HTTP_OK) {
+                            result = "BAD CONNECTION"
 
-                }
-                else{
-                    val inputStreamReader = httpURLConnection.inputStream
-                    val bufferedReader = BufferedReader(InputStreamReader(inputStreamReader))
-                    result = bufferedReader.readText()
-                    bufferedReader.close()
-                }
-            }
-            catch (e: IOException){
-                e.printStackTrace()
-            }finally {
-                httpURLConnection?.disconnect()
-            }
-            withContext(Dispatchers.Main){
-                try {
-                    val jsonObject = JSONObject(result)
-                    val valueObject = jsonObject.getJSONObject("responseData")
-                    val translatedResponse = valueObject.getString("translatedText")
-                    println(translatedResponse)
-
-                }
-                catch (e: IOException){
-                    Log.d("ERROR", e.toString())
+                        } else {
+                            val inputStreamReader = httpURLConnection.inputStream
+                            val bufferedReader =
+                                BufferedReader(InputStreamReader(inputStreamReader))
+                            result = bufferedReader.readText()
+                            bufferedReader.close()
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        httpURLConnection?.disconnect()
+                    }
+                    withContext(Dispatchers.Main) {
+                        try {
+                            val jsonObject = JSONObject(result)
+                            val valueObject = jsonObject.getJSONObject("responseData")
+                            val translatedResponse = valueObject.getString("translatedText")
+                            // put translatedResponse into db we only have to fetchOurSpanishWords once
+                            println(translatedResponse)
+                            databaseHelper.putItemsIntoDB(ourInputs[index], translatedResponse, 0)
+                        } catch (e: IOException) {
+                            Log.d("ERROR", e.toString())
+                        }
+                    }
+                    ++index
                 }
             }
         }
+    }
+
+    fun actOneSubmitButton(view: View) {
+        // grab query function to see if we are correct or not
+        // if not do dialog to show they are incorrect
+        // if correct give the next input
+        // if all words have been correct give the user a dialog that they completed level one and finish()
     }
 }
